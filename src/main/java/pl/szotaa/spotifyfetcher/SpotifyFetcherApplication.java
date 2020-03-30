9956b1989cd674cc5c9de143117a561746b55aad
szotaa <kubaszota97@gmail.com>
2020-03-30 22:38:37 +0200

    Set web env to none

--- src/main/java/pl/szotaa/spotifyfetcher/SpotifyFetcherApplication.java
+++ src/main/java/pl/szotaa/spotifyfetcher/SpotifyFetcherApplication.java
@@ -1,7 +1,8 @@
 package pl.szotaa.spotifyfetcher;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -11,7 +12,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SpotifyFetcherApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpotifyFetcherApplication.class, args);
+		new SpringApplicationBuilder(SpotifyFetcherApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
 	}
 
 }
